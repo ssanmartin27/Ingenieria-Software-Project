b1b435d54894a7b6687d59ff9f2b301242eeddb2
ssanmartin27 <95110845+ssanmartin27@users.noreply.github.com>
2024-05-24 11:58:35 -0500

    adjasdjan

--- src/AutomationControl.jsx
+++ src/AutomationControl.jsx
@@ -14,17 +14,17 @@ function OptionAutomationControl() {
               <a style={{ color: 'grey' }} href='#'>Apollo</a>
               <a style={{ color: 'grey' }} href='#'>BFT</a>
               <a style={{ color: 'grey' }} href='#'>Power Master</a>
-              <a style={{ color: 'grey' }} href='#'>Platinium Gate Openers</a>
-              <a style={{ color: 'grey' }} href='#'>Liftmaster</a>
+              <a style={{ color: 'grey' }} href='/PlatinumGateOpeners'>Platinium Gate Openers</a>
+              <a style={{ color: 'grey' }} href='/LiftMaster'>Liftmaster</a>
               <a style={{ color: 'grey' }} href='#'>Elite</a>
               <a style={{ color: 'grey' }} href='#'>Doorking</a>
             </div>
             <div style={{ marginLeft: '4rem', display: 'flex', flexDirection: 'column' }}>
-              <a style={{ color: 'grey' }} href='#'>OSCO</a>
-              <a style={{ color: 'grey' }} href='#'>Allstar</a>
+              <a style={{ color: 'grey' }} href='/OSCO'>OSCO</a>
+              <a style={{ color: 'grey' }} href='/Allstar'>Allstar</a>
               <a style={{ color: 'grey' }} href='#'>Viking</a>
               <a style={{ color: 'grey' }} href='#'>Eagle</a>
-              <a style={{ color: 'grey' }} href='#'>SEA</a>
+              <a style={{ color: 'grey' }} href='/SEA'>SEA</a>
               <a style={{ color: 'grey' }} href='#'>Hysecurity</a>
             </div>
           </div>
@@ -33,31 +33,31 @@ function OptionAutomationControl() {
         </div>
         <div style={{ marginLeft: '8rem', display: 'flex', flexDirection: 'column' }}>
           <a style={{ fontWeight: 'bold', color: 'black' }} href='#'>Remote Controls</a>
-          <a style={{ color: 'grey' }} href='#'>Multi-Code</a>
+          <a style={{ color: 'grey' }} href='/Multi-Code'>Multi-Code</a>
           <a style={{ color: 'grey' }} href='#'>Transmitter Solutions</a>
           <a style={{ color: 'grey' }} href='#'>Transmitter Solutions</a>
           <a style={{ color: 'grey' }} href='#'>Linear</a>
-          <a style={{ color: 'grey' }} href='#'>LiftMaster</a>
+          <a style={{ color: 'grey' }} href='/LiftMaster'>LiftMaster</a>
           <a style={{ color: 'grey' }} href='#'>Doorking</a>
           <a style={{ color: 'grey' }} href='#'>Keyston - Heddolf</a>
           <a style={{ color: 'grey' }} href='#'>BFT - SEA - FAAC</a>
-          <a style={{ color: 'grey' }} href='#'>Long Range</a>
+          <a style={{ color: 'grey' }} href='/Long-Range'>Long Range</a>
         </div>
         <div style={{ display: 'flex' }}>
           <div style={{ marginLeft: '8rem', display: 'flex', flexDirection: 'column' }}>
             <a style={{ fontWeight: 'bold', color: 'black' }} href='#'>Receivers</a>
             <a style={{ color: 'grey' }} href='#'>Linear/Multi-Code</a>
-            <a style={{ color: 'grey' }} href='#'>Doorking</a>
-            <a style={{ color: 'grey' }} href='#'>LiftMaster</a>
+            <a style={{ color: 'grey' }} href='/Receivers'>Doorking</a>
+            <a style={{ color: 'grey' }} href='/LiftMaster'>LiftMaster</a>
             <a style={{ color: 'grey' }} href='#'>Keystone/Heddolf</a>
-            <a style={{ color: 'grey' }} href='#'>Long Range</a>
+            <a style={{ color: 'grey' }} href='/Long-Range'>Long Range</a>
           </div>
           <div style={{ marginLeft: '8rem', display: 'flex', flexDirection: 'column' }}>
             <a style={{ fontWeight: 'bold', color: 'black' }} href='#'>Telephone Entry</a>
             <a style={{ color: 'grey' }} href='#'>Select Engineering</a>
             <a style={{ color: 'grey' }} href='#'>Doorking</a>
             <a style={{ color: 'grey' }} href='#'>LiftMaster</a>
-            <a style={{ color: 'grey' }} href='#'>Sentex</a>
+            <a style={{ color: 'grey' }} href='/Sentex'>Sentex</a>
             <a style={{ color: 'grey' }} href='#'>Elite</a>
             <a style={{ color: 'grey' }} href='#'>Linear</a>
             <a style={{ color: 'grey' }} href='#'>Readers</a>
--- src/index.js
+++ src/index.js
@@ -6,6 +6,15 @@ import ModernDrivewayGates from './ModernDrivewayGates';
 import PrivacyGardenGates from './PrivacyGardenGates';
 import Fencing from './Fencing';
 import ModernBalconyRailings from './ModernBalconyRailings';
+import Osco from './Osco';
+import PlatinumGateOpeners from './PlatinumGateOpeners';
+import Allstar from './Allstar';
+import Sea from './Sea';
+import Remote from './Remote';
+import Long from './Long';
+import Receiver from './Receiver'
+import LiftMaster from './LiftMaster';
+import Sentex from './Sentex';
 import '@mantine/core/styles.css';
 import { createTheme, MantineProvider } from '@mantine/core';
 
@@ -20,6 +29,15 @@ root.render(
         <Route path="/PrivacyGardenGates" element={<PrivacyGardenGates />} />
         <Route path="/Fencing" element={<Fencing />} />
         <Route path="/ModernBalconyRailings" element={<ModernBalconyRailings />} />
+        <Route path="/PlatinumGateOpeners" element={<PlatinumGateOpeners />} />
+        <Route path="/OSCO" element={<Osco />} />
+        <Route path="/Allstar" element={<Allstar />} />
+        <Route path="/SEA" element={<Sea />} />
+        <Route path="/Multi-Code" element={<Remote />} />
+        <Route path="/Long-Range" element={<Long />} />
+        <Route path="/Receivers" element={<Receiver />} />
+        <Route path="/LiftMaster" element={<LiftMaster />} />
+        <Route path="/Sentex" element={<Sentex />} />
       </Routes>
     </Router>
   </React.StrictMode>
